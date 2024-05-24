80d0e1ed70f03f628c75c38b66d04daf2913ebe9
woelte <woelte@bub-group.com>
2024-05-24 07:58:39 +0200

    change package name

--- bub_logger/bub_logger.py
+++ bub_logger/bub_logger.py
@@ -89,12 +89,9 @@ class BubLogger:
         config_data = None
         for ext in ["json", "yaml", "yml"]:
             try:
-                if hasattr(sys, "_MEIPASS"):
-                    config_file_path = os.path.join(sys._MEIPASS, "configs", f"{config_file}.{ext}")
-                else:
-                    config_file_path = pkg_resources.files("bub_logger.configs").joinpath(
-                    f"{config_file}.{ext}"
-                )
+                config_file_path = pkg_resources.files("bub_logger").joinpath(
+                        f"configs/{config_file}.{ext}"
+                    )
                 with config_file_path.open("r") as file:
                     config_data = file.read()
                     if ext == "json":
